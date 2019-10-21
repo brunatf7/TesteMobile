f748c696dccb4d1bf27744cbe2066804e3cda8b7
Bruna Tavares <brunatfb7@gmail.com>
2019-10-20 20:16:37 -0400

    config: Configuraçõe iniciais do projeto para consumo de web service

--- app/src/main/java/com/labtecs/bruna/testemobile/Activitys/MainActivity.java
+++ app/src/main/java/com/labtecs/bruna/testemobile/Activitys/MainActivity.java
@@ -1,4 +1,4 @@
-package com.labtecs.bruna.testemobile;
+package com.labtecs.bruna.testemobile.Activitys;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.labtecs.bruna.testemobile.R;
 
 public class MainActivity extends AppCompatActivity {
 

2ec274823c0880298ce1e53cb246ca09645ff36f
Bruna Tavares <brunatfb7@gmail.com>
2019-10-23 15:46:55 -0400

    fix: Corrigindo URL base do servico

--- app/src/main/java/com/labtecs/bruna/testemobile/Services/TMDbSearchService.java
+++ app/src/main/java/com/labtecs/bruna/testemobile/Services/TMDbSearchService.java
@@ -1,19 +1,20 @@
 package com.labtecs.bruna.testemobile.Services;
 
 import com.labtecs.bruna.testemobile.Global;
-import com.labtecs.bruna.testemobile.Objects.Movie;
+import com.labtecs.bruna.testemobile.Objects.Popular;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface TMDbSearchService {
-    public static final String BASE_URL = "https://image.tmdb.org/t/p/original/";
+    public static final String BASE_URL = "https://api.themoviedb.org/3/search/";
 
 
-    @GET("movie?query={query}&language=pt-BR&api_key=" + Global.API_KEY)
-    Call<Movie> search(@Path("query") String query);
+    @GET("movie?language=pt-BR&api_key=" + Global.API_KEY)
+    Call<Popular> search(
+            @Query("query") String query
+    );
 
 
 }
